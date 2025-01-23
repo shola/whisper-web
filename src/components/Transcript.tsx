d3275e0d571a0e44f2200c53f8e1d7d77db72e2e
shola <michael.o.situ@gmail.com>
2025-01-22 16:38:46 -0800

    fix: gate enhanced behavior behind the `ollamaAvailable` flag

--- src/components/Transcript.tsx
+++ src/components/Transcript.tsx
@@ -94,10 +94,10 @@ export default function Transcript({ transcribedData }: Props) {
             {transcribedData && !transcribedData.isBusy && (
                 <div className='w-full text-right'>
                     <button
-                        onClick={() => exportTXT(true)}
+                        onClick={() => exportTXT(ollamaAvailable)}
                         className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 inline-flex items-center'
                     >
-                        {isGrammarizing ? (
+                        {ollamaAvailable && isGrammarizing ? (
                             <Spinner text='Grammarizing...' />
                         ) : (
                             <span>
